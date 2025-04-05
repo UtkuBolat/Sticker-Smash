4b8b0342fe21b6ead651ef0fef4c16d661285968
Elfo <utkubolat@outlook.com>
2025-04-05 20:54:11 +0300

    a

---
+++ app/+not-found.tsx
@@ -0,0 +1,35 @@
+import { View, StyleSheet, Text } from 'react-native';
+import { Link, Stack } from 'expo-router';
+import { useRouter } from 'expo-router';
+
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops! Not Found' }} />
+      <View style={styles.container}>
+      <Link href={"../"} style={styles.button}>
+  <Text style={styles.button}>Go back to Home screen!</Text>
+</Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    padding: 10,
+    backgroundColor: '#007AFF',
+    color: '#fff',
+    borderRadius: 5,
+    textAlign: 'center',
+  },
+});
